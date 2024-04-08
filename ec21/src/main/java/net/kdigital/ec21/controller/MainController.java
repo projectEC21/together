6cd2f6cef9f540747ff6da114489491e004ac1f8
Inyoung <2nnnn0000@gmail.com>
2024-04-08 16:55:09 +0900

    인영 템플릿이랑 스프링 합쳐서 폴더정리

--- ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
+++ ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
@@ -3,13 +3,10 @@ package net.kdigital.ec21.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class MainController {
-    
-    @GetMapping({"/",""})
-    public String index() {
-        return "";
-    }
-    
+	@GetMapping({"","/"})
+	public String index() {
+		return ("index");
+	}
 }
