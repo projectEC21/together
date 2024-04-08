94778b7e97d4a6def5d7896191592d0fb909ffab
doyeon <kdoy030@gmail.com>
2024-04-08 16:18:59 +0900

    메인 컨트롤러 생성

---
+++ ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
@@ -0,0 +1,15 @@
+package net.kdigital.ec21.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    
+    @GetMapping({"/",""})
+    public String index() {
+        return "";
+    }
+    
+}
