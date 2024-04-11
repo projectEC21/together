bd3d4e7293442a7298cc87776fe768dbdf5604fb
doyeon <kdoy030@gmail.com>
2024-04-11 20:32:22 +0900

    240411 : DDL 작성

--- ec21/DDL.sql
+++ ec21/DDL.sql
@@ -16,8 +16,8 @@ create table customer (
     comp_desc varchar2(2000),
     comp_url varchar2(50),
     address varchar2(100) not null,
-    remote_ip varchar2(20) not null,
-    country varchar2(20),
+    remote_ip varchar2(30) not null,
+    country varchar2(30),
     create_date date default sysdate,
     update_date date,
     roles varchar2(10) default 'ROLE_USER' check(roles in ('ROLE_USER','ROLE_ADMIN')),
@@ -35,8 +35,8 @@ drop table inquiry_block;
 drop sequence inquiry_block_seq;
 
 create table inquiry_block(
-    inquiry_block number primary key,
-    customer_id varchar2(20) references customer(customer_id) on delete casecade,
+    inquiry_block_id number primary key,
+    customer_id varchar2(20) references customer(customer_id) on delete cascade,
     blocked_id varchar2(20)
 );
 
@@ -53,8 +53,8 @@ drop sequence report_customer_seq;
 create table report_customer(
     report_customer_id number primary key,
     reported_id varchar2(20) not null,
-    report_category varchar2(20) not null check(report_category in ('drug', 'IPR','spam','etc')),
-    report_reason varchar2(5000),
+    report_category varchar2(20) not null check(report_category in ('DRUG', 'IPR','SPAM','ETC')),
+    report_reason varchar2(2000),
     report_date date default sysdate,
     manager_check char(1) default 'N' check(manager_check in ('N','Y'))
 );
@@ -74,10 +74,10 @@ create table blacklist(
     blacklist_id number primary key,
     customer_id varchar2(20) not null,
     comp_name varchar2(100) not null,
-    remote_ip varchar2(20) not null,
-    country varchar2(20),
+    remote_ip varchar2(30) not null,
+    country varchar2(30),
     black_type varchar2(20) not null check(black_type in ('drug', 'IPR','spam','etc')),
-    black_reason varchar2(5000),
+    black_reason varchar2(2000),
     input_date date default sysdate
 );
 
@@ -97,16 +97,17 @@ CREATE TABLE product
     product_id VARCHAR2(50) PRIMARY KEY  
     , product_name VARCHAR2(200) NOT NULL 
     , product_desc CLOB NOT NULL
-    , product_img VARCHAR2(5000) NOT NULL
+    , original_file_name varchar2(200)
+    , saved_file_name varchar2(200)
     , price NUMBER NOT NULL
     , origin VARCHAR2(30) NOT NULL
     , moq NUMBER NOT NULL
     , unit VARCHAR2(10) NOT NULL
-    , category VARCHAR2(20) NOT NULL check(category in ('Food&Beverage','Chemical','Health&Medical','Electronic','Cosmetic'))
+    , category VARCHAR2(20) NOT NULL check(category in ('FOOD_BEVERAGE','CHEMICAL','HEALTH_MEDICAL','ELECTRONIC','COSMETIC'))
     , create_date date DEFAULT sysdate
     , update_date date
-    , remote_ip VARCHAR2(20) NOT NULL
-    , country VARCHAR2(50)
+    , remote_ip VARCHAR2(30) NOT NULL
+    , country VARCHAR2(30)
     , hit_count NUMBER DEFAULT 0
     , lstm_predict_proba NUMBER(5,2) NOT NULL
     , lstm_predict CHAR(1)  NOT NULL check(lstm_predict in ('0','1'))
@@ -121,20 +122,15 @@ select * from product;
 
 -- 금지어 (prohibit_word)
 DROP TABLE prohibit_word;
-drop sequence prohibit_word_seq;
 
 CREATE TABLE prohibit_word
 (
-    prohibit_word_id NUMBER PRIMARY KEY
+    prohibit_word VARCHAR2(200) primary key
     , prohibit_reason VARCHAR2(20) NOT NULL check(prohibit_reason in ('IPR','drug','prohibited_items','explicit_adult'))
-    , prohibit_word VARCHAR2(200) NOT NULL
 );
 
-create sequence prohibit_word_seq;
 
 select * from prohibit_word; 
-
-
 
 
 
@@ -147,7 +143,7 @@ CREATE TABLE prohibit_similar_word
     prohibit_similar_id NUMBER PRIMARY KEY
     , similar_word VARCHAR2(100) NOT NULL
     , similar_proba NUMBER(5,2) NOT NULL
-    , prohibit_word_id NUMBER references prohibit_word(prohibit_word_id) on delete casecade
+    , prohibit_word NUMBER VARCHAR2(200) prohibit_word(prohibit_word) on delete casecade
     , product_id VARCHAR2(50) references product(product_id) on delete casecade
 );
 create sequence prohibit_similar_word_seq;
@@ -166,9 +162,10 @@ create table inquiry(
     product_id varchar2(30) not null references product(product_id) on delete casecade,
     quantity number,
     inquiry_title varchar2(1000) not null,
-    inquiry_content varchar2(5000) not null,
+    inquiry_content varchar2(3000) not null,
     send_date date default sysdate,
-    file varchar2(100)
+    original_file_name varchar2(200),
+    saved_file_name varchar2(200),
     saved char(2) default 'NN' check(saved in ('NN','NY','YN','YY')),
     trash char(2) default 'NN' check(trash in ('NN','NY','YN','YY')),
     spam char(2) default 'NN' check(spam in ('NN','NY','YN','YY'))
