0625d25b22cf2cd549535e0f3e3e293c92efa13f
doyeon <kdoy030@gmail.com>
2024-04-12 12:45:48 +0900

    delete

--- ec21/src/main/java/net/kdigital/ec21/dto/check/InquiryEnum.java
+++
@@ -1,5 +0,0 @@
-package net.kdigital.ec21.dto.check;
-
-public enum InquiryEnum {
-    NN, NY, YN, YY
-}
--- ec21/src/main/java/net/kdigital/ec21/dto/check/ProhibitReason.java
+++
@@ -1,5 +0,0 @@
-package net.kdigital.ec21.dto.check;
-
-public enum ProhibitReason {
-    IPR, drug, prohibited_items, explicit_adult
-}
