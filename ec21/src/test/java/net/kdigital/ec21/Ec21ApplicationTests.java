2e1abd9324e3fa7542c10530c24c8e431425a4f2
doyeon <kdoy030@gmail.com>
2024-04-12 13:21:00 +0900

    240412 ec21 파일 생성

---
+++ ec21/src/test/java/net/kdigital/ec21/Ec21ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kdigital.ec21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
