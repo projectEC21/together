9752cae3600d851a86760126b8b0b21d87ed1ec8
Yeonnnny <kdoy030@gmail.com>
2024-04-14 17:36:00 +0900

    240414 : 모델로 판단된 이상상품 페이지 기능 구현

--- ec21/src/main/java/net/kdigital/ec21/repository/ProductRepository.java
+++ ec21/src/main/java/net/kdigital/ec21/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package net.kdigital.ec21.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,4 +14,10 @@ public interface ProductRepository extends JpaRepository<ProductEntity, String> 
         // 당일 등록된 이상 상품 개수
         Long countByCreateDateBetweenAndLstmPredict(LocalDateTime startDate, LocalDateTime endDate,
                         boolean lstmPredict);
+
+        // lstmPredict 값이 이상 즉 false(0)인 모든 ProductEntity 리스트로 반환
+        List<ProductEntity> findByLstmPredict(boolean lstmPredict);
+
+        // lstmPredict 값이 이상 즉 false(0)이고 judge가 null(관리자 미처리)인 모든 ProductEntity 리스트 반환 (최신 상품 등록일 순으로)
+        List<ProductEntity> findByLstmPredictAndJudgeOrderByCreateDateDesc(boolean lstmPredict, String judgeIsNull);
 }
