8a61367e8e06a7ed57fe11bf99a2fd306a524546
Heejin <heejiningermany@gmail.com>
2024-04-15 17:45:06 +0900

    풀전 커밋

--- ec21/src/main/java/net/kdigital/ec21/controller/ManagerController.java
+++ ec21/src/main/java/net/kdigital/ec21/controller/ManagerController.java
@@ -63,22 +63,22 @@ public class ManagerController {
 	}
 
 	/**
-	 * ajax - 전체 상품 화면에서 상품 리스트 반환 
+	 * ajax - 전체 상품 화면에서 상품 리스트 반환
 	 * (카테고리, 검색어 조건에 따라 다르게 반환, 기본적으로 최신 등록일자 순으로 반환)
-	 * (아직 버튼 처리 안함) 
+	 * (아직 버튼 처리 안함)
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping(value = "/manager/productList/getList", method = RequestMethod.GET)
-	public String getProductList(@RequestParam(name = "category" , defaultValue = "total")String category,
-		@RequestParam(name = "searchWord" , defaultValue = "") String searchWord, Model model) {
-				
+	public String getProductList(@RequestParam(name = "category", defaultValue = "total") String category,
+			@RequestParam(name = "searchWord", defaultValue = "") String searchWord, Model model) {
+
 		List<ProductDTO> dtoList = managerService.selectProductBySearch(category, searchWord);
 		model.addAttribute("list", dtoList);
 
 		return "/manager/productList::#result";
 	}
-
 
 	/**
 	 * 모델로 이상상품 판별된 상품 리스트 화면 요청
@@ -94,20 +94,19 @@ public class ManagerController {
 	 * ajax - 모델이 이상으로 판별하고 관리자가 아직 처리하지 않은 상품 (lstm_predict==0 && judge==null) 중
 	 * 전달 받은 카테고리와 검색어에 해당하는 상품을 최신 등록일 순으로 반환
 	 * (아직 버튼 처리 안함)
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping(value = "/manager/modelPredict/getList", method = RequestMethod.GET)
 	public String getList(@RequestParam(name = "category", defaultValue = "total") String category,
 			@RequestParam(name = "searchWord", defaultValue = "") String searchWord, Model model) {
-		
+
 		List<ModelPredictDTO> dtoList = managerService.selectModelPredictWeirdBySearch(category, searchWord);
 		model.addAttribute("list", dtoList);
 
 		return "/manager/modelPredict::#result";
 	}
-
-
 
 	// ============================= 회원 관리 =============================
 
