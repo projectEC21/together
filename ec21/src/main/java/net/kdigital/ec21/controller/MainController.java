9242cd6d31247cc634ce1e057d0b699ddc1a3bb1
Heejin <heejiningermany@gmail.com>
2024-04-15 15:07:11 +0900

    수정

--- ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
+++ ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping({ "", "/" })
-	public String index() {
-		return "manager/manager_index";
-	}
-
 	// @GetMapping({ "", "/" })
 	// public String index() {
 	// return "manager/manager_index";
