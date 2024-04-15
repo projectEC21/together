abae966a9af96fa70b62df3c9916fec36e7939b3
Heejin <heejiningermany@gmail.com>
2024-04-15 14:27:51 +0900

    잠시 커밋

--- ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
+++ ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping({ "", "/" })
-	public String index() {
-		return "manager/manager_index";
-	}
 	// @GetMapping({ "", "/" })
 	// public String index() {
-	// return "main/register";
+	// return "manager/manager_index";
 	// }
+	@GetMapping({ "", "/" })
+	public String index() {
+		return "main/";
+	}
 }
