d568285db7563260ca95841b79594091f8080908
Heejin <heejiningermany@gmail.com>
2024-04-16 11:19:43 +0900

    ++

--- ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
+++ ec21/src/main/java/net/kdigital/ec21/controller/MainController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package net.kdigital.ec21.controller;
 
 import org.springframework.stereotype.Controller;
@@ -15,21 +14,3 @@ public class MainController {
 		return "main/index";
 	}
 }
-=======
-package net.kdigital.ec21.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MainController {
-	// @GetMapping({ "", "/" })
-	// public String index() {
-	// return "manager/manager_index";
-	// }
-	@GetMapping({ "", "/" })
-	public String index() {
-		return "main/index";
-	}
-}
->>>>>>> 15fb48aec3b88996044c30b04e07a2761ed1910d
