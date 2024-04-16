95670fa5f7ff8b25c7f6e0e53e1721c9cfa44b88
Heejin <heejiningermany@gmail.com>
2024-04-16 11:04:51 +0900

    no message

--- ec21/src/main/java/net/kdigital/ec21/repository/BlacklistRepository.java
+++
@@ -1,9 +0,0 @@
-package net.kdigital.ec21.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.kdigital.ec21.entity.BlacklistEntity;
-
-public interface BlacklistRepository extends JpaRepository<BlacklistEntity, Long> {
-
-}
--- ec21/src/main/java/net/kdigital/ec21/repository/InquiryBlockRepository.java
+++
@@ -1,9 +0,0 @@
-package net.kdigital.ec21.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.kdigital.ec21.entity.InquiryBlockEntity;
-
-public interface InquiryBlockRepository extends JpaRepository<InquiryBlockEntity, Long>{
-    
-}
