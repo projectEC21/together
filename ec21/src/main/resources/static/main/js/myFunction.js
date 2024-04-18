613cd3a8205c7e40936b480ac9f0897d7e687736
juyeong <yp9782@gmail.com>
2024-04-18 11:39:53 +0900

    Edit. modal 수정중

--- ec21/src/main/resources/static/main/js/myFunction.js
+++ ec21/src/main/resources/static/main/js/myFunction.js
@@ -1,7 +1,8 @@
 // 모달과 관련된 함수
 
+// modalControl.js
 $(document).ready(function() {
     $('#inquiry').click(function() {
-        $('#myModal').modal('show');  // Bootstrap 모달을 열기
+        $('#myModal').modal('show');  // Bootstrap 모달을 수동으로 열기
     });
 });
