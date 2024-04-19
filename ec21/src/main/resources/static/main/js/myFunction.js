dbfcb25d77a302eec8847b7493bb78a8a3ff995a
juyeong <yp9782@gmail.com>
2024-04-19 15:59:47 +0900

    Edit. 인콰이어리 수정중

--- ec21/src/main/resources/static/main/js/myFunction.js
+++ ec21/src/main/resources/static/main/js/myFunction.js
@@ -7,3 +7,14 @@ $(document).ready(function() {
     });
 });
 
+// saved-icon 을 누르면 색이 있는 별로 되게 하는 스크립트
+$(document).ready(function() {
+    $('#saved-icon').click(function(event) {
+        event.preventDefault(); // 기본 동작 방지
+        var icon = $(this).find('i'); // .saved-icon 내부의 <i> 태그를 찾음
+    
+        // 클래스 변경
+        icon.removeClass('ti-star'); // 기존 클래스 제거
+        icon.addClass('fa fa-star'); // 새 클래스 추가
+    });
+});
