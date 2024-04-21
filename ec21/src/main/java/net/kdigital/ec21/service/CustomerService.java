b8f226a7b6e50f84a682f835db034ec235dc8d7e
doyeon <kdoy030@gmail.com>
2024-04-21 19:12:30 +0900

    240421 dy : register 회원가입 처리 완료, 유효성 검사 미완료git push!

--- ec21/src/main/java/net/kdigital/ec21/service/CustomerService.java
+++ ec21/src/main/java/net/kdigital/ec21/service/CustomerService.java
@@ -1,20 +1,20 @@
 package net.kdigital.ec21.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.kdigital.ec21.dto.CustomerDTO;
-import net.kdigital.ec21.dto.ProductDTO;
 import net.kdigital.ec21.entity.CustomerEntity;
-import net.kdigital.ec21.entity.ProductEntity;
 import net.kdigital.ec21.repository.CustomerRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    // private final BCryptPasswordEncoder bCryptPasswordEncoder; // 비밀번호 암호화
 
     /**
      * 회원 ID를 전달받아 회원DTO를 반환하는 함수
@@ -30,9 +30,19 @@ public class CustomerService {
      * 회원 등록 - 전달 받은 회원DTO를 Entity로 변환하여 DB에 저장하는 함수
      * @param customerDTO
      */
-    public void insertCustomer(CustomerDTO customerDTO) {
+    public Boolean registerProc(CustomerDTO customerDTO) {
+        Boolean isExist = customerRepository.existsById(customerDTO.getCustomerId());
+
+        if (isExist) return false;
+        
+        // // 비밀번호 암호화
+        // customerDTO.setCustomerPw(bCryptPasswordEncoder.encode(customerDTO.getCustomerPw()));
+        // DTO -> entity
         CustomerEntity entity = CustomerEntity.toEntity(customerDTO);
+        // DB 저장
         customerRepository.save(entity);
+
+        return true;
     }
 
     /**
@@ -45,6 +55,7 @@ public class CustomerService {
         CustomerEntity entity = customerRepository.findById(dto.getCustomerId()).get();
 
         // 바꿀수 없는 값 : ID
+        // entity.setCustomerPw(bCryptPasswordEncoder.encode(dto.getCustomerPw()));
         entity.setCustomerPw(dto.getCustomerPw());
         entity.setCustomerName(dto.getCustomerName());
         entity.setEmail(dto.getEmail());
@@ -60,6 +71,19 @@ public class CustomerService {
         entity.setTradeNo(dto.getTradeNo());
 
         return CustomerDTO.toDTO(entity);
+    }
+
+    /**
+     * DB에 전달받은 회원 ID가 존재하는지 확인하는 함수
+     * @param customerId
+     * @return
+     */
+    public Boolean isExistId(String customerId) {
+        Optional<CustomerEntity> entity =  customerRepository.findById(customerId);
+        if (entity.isPresent()) {
+            return true;
+        }
+        return false;
     }
 
     
