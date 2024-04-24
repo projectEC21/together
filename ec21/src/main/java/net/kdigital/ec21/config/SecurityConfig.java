bdd81ff62926ac14bca0a056213828b391e62f33
김인영 <>
2024-04-24 20:36:37 +0900

    no message

--- ec21/src/main/java/net/kdigital/ec21/config/SecurityConfig.java
+++ ec21/src/main/java/net/kdigital/ec21/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests((auth) -> auth.requestMatchers("/",
                 "/main/index",
                 "/manager/manager_index",
-                "manager/**",
+                "/manager/**",
                 "/main/register",
                 "/main/login",
                 "/main/list",
