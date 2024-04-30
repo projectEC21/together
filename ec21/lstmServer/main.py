747281ab8fa7e7f4f93f40c0f767b3cff19d8d95
juyeong <yp9782@gmail.com>
2024-04-30 15:47:42 +0900

    +++

--- ec21/lstmServer/main.py
+++ ec21/lstmServer/main.py
@@ -62,8 +62,9 @@ import random
 # oracleDB 연결
 import oracledb 
 
-oracledb.init_oracle_client(lib_dir="C:\DownloadFiles\OracleInstantClient\instantclient_19_22") #도연경로
+#oracledb.init_oracle_client(lib_dir="C:\DownloadFiles\OracleInstantClient\instantclient_19_22") #도연경로
 # oracledb.init_oracle_client(lib_dir="/Users/inn00/Downloads/instantclient_19/'instantclient_19_16 basic'") # 인영경로
+oracledb.init_oracle_client(lib_dir="/Users/juyeong/Downloads/instantclient_19/'instantclient_19_16 basic") # 주영경로
 # 맥은 cx_Oracle 사용해야하는거 같아유
 con = oracledb.connect(user='admin', password='DIMA3project', dsn='h2z0kxkxxnhhftv2_high')
 
